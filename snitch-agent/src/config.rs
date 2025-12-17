0e8b7bb8d0afa0a1a7e014bd00e80907e8a75d71
gebz <gebz@gebz.local>
2025-12-17 08:13:39 +0200

    Added some stuff

--- snitch-agent/src/config.rs
+++ snitch-agent/src/config.rs
@@ -1,8 +1,6 @@
 use anyhow::Result;
-use serde::Deserialize;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_yaml::from_str;
-use std::path;
 use std::fs;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -11,12 +9,21 @@ pub struct AgentConfiguration {
     aggregator_host: String,
     aggregator_port: i16,
     max_retries: i16,
-    log_level: String
+    log_level: LogLevel,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warn,
+    Error,
 }
 
 pub fn load_config(config: String) -> Result<AgentConfiguration> {
     let config_file_contents = fs::read_to_string(config)?;
     println!("Contents:\n{}", config_file_contents);
-    let agent_config :AgentConfiguration = from_str(&config_file_contents)?;
+    let agent_config: AgentConfiguration = from_str(&config_file_contents)?;
     Ok(agent_config)
 }
